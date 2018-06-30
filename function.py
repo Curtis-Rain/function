204938acf747ad79b0a2820d8b53d3ed2315e714
Curtis-Rain <2531495316@qq.com>
2018-06-30 20:07:35 +0800

    'hi'

---
+++ function.py
@@ -0,0 +1,21 @@
+def money(x):
+          num = 250 + (250 * 0.07) * x
+          return num
+
+
+print(money(1))
+print(money(3))
+print(money(7))
+print(money(20))
+
+
+def cool(a):
+         num = 325 + (325 * 0.04) * a
+         return num
+
+
+print(cool(1))
+print(cool(3))
+print(cool(7))
+print(cool(20))
+
